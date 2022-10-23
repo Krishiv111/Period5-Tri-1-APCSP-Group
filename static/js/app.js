fb6f36e62f583e37a2c8e6d67f25cd61d5bbce90
ADVAYS16 <advay1216@gmail.com>
2022-10-23 12:42:38 -0700

    comments

--- static/js/app.js
+++ static/js/app.js
@@ -6,15 +6,15 @@ window.onload = () => {
 };
 
 function calculateBMI() {
-
+    //Getting Input for Height: In Centimeters
 	let height = parseInt(document
 			.querySelector("#height").value);
-
+    //Getting Input for Weight: In Kilograms
 	let weight = parseInt(document
 			.querySelector("#weight").value);
-
+    
 	let result = document.querySelector("#result");
-
+    // Insures that height and weight input is valid: No Letters
 	if (height === "" || isNaN(height))
 		result.innerHTML = "Provide a valid Height!";
 
@@ -22,10 +22,10 @@ function calculateBMI() {
 		result.innerHTML = "Provide a valid Weight!";
 
 	else {
-
+    //Calculates BMI upto 2 Decimals Spaces
 		let bmi = (weight / ((height * height)
 							/ 10000)).toFixed(2);
-
+        // Based off Results: Classified as Underweight, Overweight, or Normal
 		if (bmi < 18.6) result.innerHTML =
 			`Under Weight : <span>${bmi}</span>`;
 
