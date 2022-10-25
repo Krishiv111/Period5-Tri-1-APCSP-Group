976d776d1178177a134f6c4ce95efd3d21c2e410
ADVAYS16 <advay1216@gmail.com>
2022-10-25 15:43:36 -0700

    bmicalc - updated

--- static/js/app.js
+++ static/js/app.js
@@ -1,40 +1,24 @@
-window.onload = () => {
-	let button = document.querySelector("#btn");
+var heightInput = document.querySelector(".height-input-field");
+var weightInput = document.querySelector(".weight-input-field");
+var calculateButton = document.querySelector(".calculate");
+var result = document.querySelector(".result");
+var statement = document.querySelector(".result-statement");
+var BMI, height, weight;
 
-	// Function for calculating BMI
-	button.addEventListener("click", calculateBMI);
-};
+calculateButton.addEventListener("click", ()=>{
 
-function calculateBMI() {
-    //Getting Input for Height: In Centimeters
-	let height = parseInt(document
-			.querySelector("#height").value);
-    //Getting Input for Weight: In Kilograms
-	let weight = parseInt(document
-			.querySelector("#weight").value);
-    
-	let result = document.querySelector("#result");
-    // Insures that height and weight input is valid: No Letters
-	if (height === "" || isNaN(height))
-		result.innerHTML = 'Provide a valid Height!:  <span style="font-style:italic;"> Proporcione una Altura Válida!"</span>';
+    height = heightInput.value;
+    weight = weightInput.value;
+    BMI = weight*700/(height*height); 
+    result.innerText = BMI;
 
-	else if (weight === "" || isNaN(weight))
-		result.innerHTML = 'Provide a valid Weight!: <span style="font-style:italic;">Proporcione una Peso Válida!"</span>';
-
-	else {
-    //Calculates BMI upto 2 Decimals Spaces
-		let bmi = (weight / ((height * height)
-							/ 10000)).toFixed(2);
-        // Based off Results: Classified as Underweight, Overweight, or Normal
-		if (bmi < 18.6) result.innerHTML =
-			`Under Weight: <span style="font-style:italic;"> Bajo Peso </span> : <span>${bmi}</span>`;
-
-		else if (bmi >= 18.6 && bmi < 24.9)
-			result.innerHTML =
-				`Normal: <span style="font-style:italic;"> Normal </span> : <span>${bmi}</span>`;
-
-		else result.innerHTML =
-			`Over Weight : <span style="font-style:italic;"> Exceso de Peso </span>: <span>${bmi}</span>`;
-	}
-}
-
+    if(BMI < 18.5){
+        statement.innerText = "Underweight: Baja Peso";    
+    }else if((BMI > 18.5) && (BMI < 24.9)){
+        statement.innerText = "Normal";
+    }else if((BMI > 25) && (BMI < 29.9 )){
+        statement.innerText = "Overweight: Exceso de Peso";
+    }else{
+        statement.innerText = "Obese: Obeso";
+    }
+});
