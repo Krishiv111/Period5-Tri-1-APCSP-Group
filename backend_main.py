616466707611f88d00004839e8d4e3e6dc7ad197
DeDuva4 <dhruvasiyer@gmail.com>
2022-10-26 15:39:28 -0700

    fitness API

--- backend_main.py
+++ backend_main.py
@@ -1,44 +1,15 @@
-s_ff_user_structure = []
+import requests
 
-s_ff_user_structure.append({
-    "Firstname": "",
-    "Lastname": "",
-    "Age": "",
-    "Height": ""
-})
+url = "https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises"
 
+querystring = {"muscle":"biceps"}
 
-# Main db of all users
-ff_db = []
-ff_db.append({
-    "Firstname": "Dhruva",
-    "Lastname": "Iyer",
-    "Age": 15,
-    "Height": 70
-})
+headers = {
+	"X-RapidAPI-Key": "825200d0f8msh414d353da41bfcfp1ddcfcjsnb40ef386fce9",
+	"X-RapidAPI-Host": "exercises-by-api-ninjas.p.rapidapi.com"
+}
 
-user0 = ({
-    "Firstname": "Kanishka",
-    "Lastname": "Iyer",
-    "Age": 13,
-    "Height": 50
-})
+response = requests.request("GET", url, headers=headers, params=querystring)
 
-
-
-# An API function that adds a new user to the db
-def add_ff_user(new_user):
-    ff_db.append(new_user)
-    
-add_ff_user(user0)
-
-# An API function that removes an existing user from the db
-# This is done by looking up their user ID
-#def del_ff_user(new_user):
-    #userid = new_user["ID"]
-    #ff_db.remove(user)
-
-
-
-length = (len(ff_db))
-print(length)
+response_text = (response.text)
+print(response_text)
