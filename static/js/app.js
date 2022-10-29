9bc8f0de30b300c722b3b180735682478d62522f
ADVAYS16 <advay1216@gmail.com>
2022-10-29 13:53:55 -0700

    finished bmi calc

--- static/js/app.js
+++ static/js/app.js
@@ -12,15 +12,15 @@ calculateButton.addEventListener("click", ()=>{
     height = height.value;
     weight = weight.value;
     BMI = weight*700/(height*height); 
-    result.innerText = BMI.toFixed(1);
+    result.innerText = "BMI : "+BMI.toFixed(1);
 
     if(BMI < 18.5){
-        statement.innerText = "Underweight: Baja Peso";    
+        statement.innerText = "Category : Underweight: Baja Peso";    
     }else if((BMI > 18.5) && (BMI < 24.9)){
-        statement.innerText = "Normal";
+        statement.innerText = "Category : Normal";
     }else if((BMI > 25) && (BMI < 29.9 )){
-        statement.innerText = "Overweight: Exceso de Peso";
+        statement.innerText = "Category : Overweight: Exceso de Peso";
     }else{
-        statement.innerText = "Obese: Obeso";
+        statement.innerText = "Category : Obese: Obeso";
     }
 });
