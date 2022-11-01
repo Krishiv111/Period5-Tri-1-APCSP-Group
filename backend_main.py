d52d1016e939582c2b63098bec2d0ba283842fe4
DeDuva4 <dhruvasiyer@gmail.com>
2022-11-01 14:58:29 -0700

    backend api

--- backend_main.py
+++ backend_main.py
@@ -1,15 +1,17 @@
 import requests
 
-url = "https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises"
+url = "https://motivational-quotes1.p.rapidapi.com/motivation"
 
-querystring = {"muscle":"biceps"}
-
+payload = {
+	"key1": "value",
+	"key2": "value"
+}
 headers = {
+	"content-type": "application/json",
 	"X-RapidAPI-Key": "825200d0f8msh414d353da41bfcfp1ddcfcjsnb40ef386fce9",
-	"X-RapidAPI-Host": "exercises-by-api-ninjas.p.rapidapi.com"
+	"X-RapidAPI-Host": "motivational-quotes1.p.rapidapi.com"
 }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
-
-response_text = (response.text)
-print(response_text)
+response = requests.request("POST", url, json=payload, headers=headers)
+random_quote = response.text
+print(random_quote)
