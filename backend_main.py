b68bc09615ebc6d5e1f352a3a157c3a5f9a77f55
DeDuva4 <dhruvasiyer@gmail.com>
2022-11-03 11:59:05 -0700

    motivation

--- backend_main.py
+++ backend_main.py
@@ -1,4 +1,15 @@
-import random
+import random, requests, json
+
+url = "https://quotes15.p.rapidapi.com/quotes/random/"
+
+headers = {
+        "X-RapidAPI-Key": "825200d0f8msh414d353da41bfcfp1ddcfcjsnb40ef386fce9",
+        "X-RapidAPI-Host": "quotes15.p.rapidapi.com"
+}
+
+response = requests.request("GET", url, headers=headers)
+output = json.loads(response.text)
+print(response.text)
 
 team_db = []
 team_names = []
@@ -38,5 +49,5 @@ for person in team_db:
 def getRandomTeamMember():
     return(random.choice(team_db))
 
-random_team_member = getRandomTeamMember()
-print(random_team_member)
+
+#print(random_team_member)
